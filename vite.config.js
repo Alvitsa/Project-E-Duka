9c4d3dd6470277ed053a806c88a3aa26baf6f645
+ <+>
2024-12-08 20:18:41 +0300

    config changes

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,27 @@
 import react from "@vitejs/plugin-react-swc";
 import autoprefixer from "autoprefixer";
-import postcss from "postcss/lib/postcss";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
     react(),
-    postcss({
-      plugins: [autoprefixer],
-      config: "./postcss.config.cjs",
-    }),
   ],
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
   build: {
     sourcemap: true,
-  },
-  define: {
-    "process.env": {},
+    chunkSizeWarningLimit: 1000, // Adjust if large dependencies exist
   },
   resolve: {
     alias: {
-      src: "/src",
+      src: resolve(__dirname, "src"),
     },
+  },
+  define: {
+    "process.env": {}, // Optional, remove if unused
   },
 });
